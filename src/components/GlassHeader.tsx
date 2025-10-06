9372911d04c33192640824be79378a0b977de13c
hitendalsaniya <hiten.qtonz@yahoo.com>
2025-10-07 02:33:21 +0530

    Hiten - 2 file changed - fix header scrolling issue for mobile view

--- src/components/GlassHeader.tsx
+++ src/components/GlassHeader.tsx
@@ -78,7 +78,7 @@ export default function GlassHeader() {
                     href={`#${item}`}
                     className="transition-colors hover:text-foreground/80 text-foreground/60 py-2"
                     onClick={toggleMenu}
-                    initial={{ opacity: 0, x: -20 }}
+                    initial={{ opacity: 0, x: -40 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.2, delay: index * 0.1 }}
                   >
